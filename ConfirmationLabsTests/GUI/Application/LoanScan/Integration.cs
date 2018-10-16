554e937afff0c57df601ee4b091555e21e21fc80
kobavadim <kobavadim@gmail.com>
2018-10-16 15:43:34 +0300

    Integration test

--- ConfirmationLabsTests/GUI/Application/LoanScan/Integration.cs
+++ ConfirmationLabsTests/GUI/Application/LoanScan/Integration.cs
@@ -3,10 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace ConfirmationLabsTests.GUI.Application.LoanScan
 {
     class Integration
     {
+        //ELEMENTS
+        private static readonly By Row = By.CssSelector(".loan-row:nth-child(1) .nowrap");
+        private static readonly By LoanAmount = By.CssSelector(".loan-row:nth-child(1) td:nth-child(3) .nowrap");
+        private static readonly By DharmaColumn = By.CssSelector(".mr-auto .btn-sm:nth-child(1)");
+        private static readonly By LaonAmountCard = By.CssSelector(".col - sm - 9:nth - child(4) > span");
+
+        //Methods
+        public static void Open()
+        {
+
+            string Env = Helpers.TestData.DefineBaseUrlDependingOnEnvironment();
+            if (Env == "PROD")
+            {
+                Console.WriteLine("running tests on PROD " + Helpers.TestData.Urls.LoanScanMainPageProd);
+                Engine.Browser.CurrentBrowser.Navigate().GoToUrl(Helpers.TestData.Urls.LoanScanMainPageProd);
+            }
+            else
+            {
+                Console.WriteLine("running tests on KOVAN " + Helpers.TestData.Urls.LoanScanMainPageKovan);
+                Engine.Browser.CurrentBrowser.Navigate().GoToUrl(Helpers.TestData.Urls.LoanScanMainPageKovan);
+            }
+
+            Engine.Browser.MiddlePause();
+
+        }
+
+        //Tests
+
+        public static void VerifySingleCardOpening()
+        {
+            Open();
+            IWebElement dharmacolumn = Engine.Browser.CurrentBrowser.FindElement(DharmaColumn);
+            dharmacolumn.Click();
+            Engine.Browser.MiddlePause();
+            
+            IWebElement firstrow = Engine.Browser.CurrentBrowser.FindElement(Row);
+            IWebElement loanamount = Engine.Browser.CurrentBrowser.FindElement(LoanAmount);
+            string amount = loanamount.Text;
+
+            string[] stringSeparators = new string[] { "$" };
+            var result = amount.Split(stringSeparators, StringSplitOptions.None);
+
+
+
+
+            firstrow.Click();
+            Engine.Browser.MiddlePause();
+
+            IWebElement loanamountcard = Engine.Browser.CurrentBrowser.FindElement(LaonAmountCard);
+            Assert.IsTrue(loanamountcard.Text.Contains(result[1]));
+
+
+        }
+
+
+
     }
 }
--- ConfirmationLabsTests/GUI/Tests/LoanScan/IntegrationTests.cs
+++ ConfirmationLabsTests/GUI/Tests/LoanScan/IntegrationTests.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using ConfirmationLabsTests.GUI.Engine;
+using ConfirmationLabsTests.GUI.Application.LoanScan;
 
 namespace ConfirmationLabsTests.GUI.Tests.LoanScan
 {
+
+
+    [TestFixture]
     class IntegrationTests
     {
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            Browser.Start();
+        }
+
+
+
+        [Category("Loanscan")]
+        [Category("Integration")]
+        [Test]
+        public void CardisOpenedfromTableTest()
+        {
+           Integration.VerifySingleCardOpening();
+        }
+
+
+
+
+        [TearDown]
+        public void TestCleanUp()
+        {
+            Browser.Close();
+        }
+
     }
 }
