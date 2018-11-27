b6f288dc5d51bf2a976702343d1707211cdebb7b
akudrytski <akudrytski@gmail.com>
2018-11-27 13:05:43 +0300

    data validation test added

---
+++ ConfirmationLabsTests/GUI/Tests/LoanScan/Importer/TermsParams.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConfirmationLabsTests.GUI.Tests.LoanScan.Importer
+{
+    public class TermsParams
+    {
+        public enum RepaymentFrequencyType
+        {
+            Hourly = 0,
+            Daily = 1,
+            Weekly = 2,
+            Monthly = 3,
+            Yearly = 4
+        }
+
+        public TimeSpan LoanTerm { get; set; }
+
+        public RepaymentFrequencyType RepaymentFrequency { get; set; }
+
+        public decimal InterestRate { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public decimal CollateralAmount { get; set; }
+
+        public string CollateralTokenAddress { get; set; }
+    }
+}
